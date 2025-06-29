d8286bd9862da340005f4844c54e86ce5a5d9caf
Danny Wharton <danny@Dannys-MacBook-Air.local>
2025-06-29 17:27:26 +0100

    view capsule logic

--- capsules/urls.py
+++ capsules/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('create-capsule/', views.create_capsule, name='create_capsule')
+    path('create-capsule/', views.create_capsule, name='create_capsule'),
+    path('capsule/<int:capsule_id>/', views.view_capsule, name='view_capsule'),
 ]
--- capsules/views.py
+++ capsules/views.py
@@ -16,31 +16,18 @@ def create_capsule(request):
         form = MessageForm()
     return render(request, 'capsules/create_capsule.html', {'form': form})
 
-# def submit_message(request):
-#     if request.method == 'POST':
-#         form = MessageForm(request.POST)
-#         if form.is_valid():
-#             message = form.save()
-#             return render(request, 'capsules/message_submitted.html', {'secret_code': message.secret_code})
-#     else:
-#         form = MessageForm()
-#     return render(request, 'capsules/submit_message.html', {'form': form})
+def view_capsule(request, capsule_id):
+    if request.method == 'GET':
+        message = get_object_or_404(Message, id=capsule_id)
 
-# def view_message(request):
-#     if request.method == 'POST':
-#         secret_code = request.POST.get('secret_code')
-#         try:
-#             message = Message.objects.get(secret_code=secret_code)
-#         except Message.DoesNotExist:
-#             return render(request, 'capsules/message_not_found.html')
-
-#         if not message.is_unlockable():
-#             return render(request, 'capsules/message_locked.html', {'unlock_date': message.unlock_date})
+        if message.is_unlocked:
+            return render(request, 'capsules/view_unlocked_capsule.html', {'message': message.content})
         
-#         return render(request, 'capsules/view_message.html', {'message': message})
-
-#     return render(request, 'capsules/enter_secret_code.html')
-
-# def all_messages(request):
-#     messages = Message.objects.all()
-#     return render(request, 'capsules/list_all_messages.html', {'messages': messages})
+        elif message.is_unlockable():
+            return render(request, 'capsules/unlock_capsule.html', {'capsule_id': message.id})
+        
+        else:
+            return render(request, 'capsules/capsule_locked.html', {'capsule_id': message.id})
+        
+    else:
+        return render(request, 'capsules/home.html')
