66aa287d23ae155580cdc0c1dec22809e6951254
Whrtn <danielwharton00@outlook.com>
2025-07-02 21:46:14 +0100

    test commit

--- capsules/views.py
+++ capsules/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Message
 from .forms import MessageForm
-
 from django.shortcuts import render, redirect
+
 
 def home(request):
     error = ""
@@ -13,12 +13,15 @@ def home(request):
 
         if not capsule_id:
             error = "Please enter a capsule ID."
+            return render(request, 'capsules/home.html', {'error': error, 'capsule_id': capsule_id}, status=400)
         elif not capsule_id.isdigit():
             error = "Invalid capsule ID. Only numbers allowed, no spaces."
+            return render(request, 'capsules/home.html', {'error': error, 'capsule_id': capsule_id}, status=400)
         else:
             return redirect('view_capsule', capsule_id=capsule_id)
 
     return render(request, 'capsules/home.html', {'error': error, 'capsule_id': capsule_id})
+
 
 def create_capsule(request):
     if request.method == 'POST':
@@ -30,6 +33,7 @@ def create_capsule(request):
         form = MessageForm()
     return render(request, 'capsules/create_capsule.html', {'form': form})
 
+
 def view_capsule(request, capsule_id):
     if request.method == 'GET':
         try:
@@ -40,12 +44,9 @@ def view_capsule(request, capsule_id):
 
         if message.is_unlocked:
             return render(request, 'capsules/view_unlocked_capsule.html', {'message': message.content})
-        
         elif message.is_unlockable():
             return render(request, 'capsules/unlock_capsule.html', {'capsule_id': message.id})
-        
         else:
             return render(request, 'capsules/capsule_locked.html', {'capsule_id': message.id, 'unlock_date': message.unlock_date})
-        
     else:
         return render(request, 'capsules/home.html')
