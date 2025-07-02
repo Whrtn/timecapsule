4ee0b120d95e199daa3d626a7c7c0c56ed0ee7a1
Whrtn <danielwharton00@outlook.com>
2025-07-02 22:48:08 +0100

    enter secret code functionality

--- capsules/views.py
+++ capsules/views.py
@@ -40,7 +40,7 @@ def view_capsule(request, capsule_id):
             message = Message.objects.get(id=capsule_id)
         except Message.DoesNotExist:
             error = "Capsule ID not found. Please try again."
-            return render(request, 'capsules/home.html', {'error': error, 'capsule_id': capsule_id})
+            return render(request, 'capsules/home.html', {'error': error, 'capsule_id': capsule_id}, status=404)
 
         if message.is_unlocked:
             return render(request, 'capsules/view_unlocked_capsule.html', {'message': message.content})
@@ -48,5 +48,27 @@ def view_capsule(request, capsule_id):
             return render(request, 'capsules/unlock_capsule.html', {'capsule_id': message.id})
         else:
             return render(request, 'capsules/capsule_locked.html', {'capsule_id': message.id, 'unlock_date': message.unlock_date})
+
+    elif request.method == 'POST':
+        try:
+            message = Message.objects.get(id=capsule_id)
+        except Message.DoesNotExist:
+            error = "Capsule ID not found. Please try again."
+            return render(request, 'capsules/home.html', {'error': error, 'capsule_id': capsule_id}, status=404)
+
+        secret_code_entered = request.POST.get('secret_code', '').strip()
+        if not secret_code_entered:
+            error = "Secret code cannot be empty."
+            return render(request, 'capsules/unlock_capsule.html', {'capsule_id': message.id, 'error': error})
+
+        if secret_code_entered.strip() == str(message.secret_code).strip():
+            message.is_unlocked = True
+            message.save()
+
+            return render(request, 'capsules/view_unlocked_capsule.html', {'message': message.content})
+        else:
+            error = "Incorrect Secret Code."
+            return render(request, 'capsules/unlock_capsule.html', {'capsule_id': message.id, 'error': error})
+
     else:
         return render(request, 'capsules/home.html')
